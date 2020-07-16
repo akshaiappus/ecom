3c1b61636a5b2402bbcada89a453f312db4c4ea9
akshaiappus <akshairamesh17@gmail.com>
2020-07-16 10:09:17 +0530

    new error

--- users.js
+++ users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { getMaxListeners } = require('process');
 class usersRepository {
     constructor(filename){
         if(!filename){
@@ -13,17 +14,26 @@ class usersRepository {
             fs.writeFileSync(this.filename,'[]');
         }
     }
- async getAll(){
-  console.log(this.filename);
-  const contents = await fs.promises.readFile(this.filename,{ encoding : 'utf8'});
-  console.log(contents);
+   getAll(){
+  return JSON.parse(fs.readFileSync(this.filename,{ encoding : 'utf8'})
+  );
+  
  }
-    
 }
+ 
+ create(attr){
+  const records = repo.getAll();
+  records.push(attr);
+   fs.writeFileSync("./users.json", JSON.stringify(records));
+ }   
 
-const test = async () => {
+
+
+const test = () => {
 const repo = new usersRepository('users.json');
-await repo.getAll();
-};
+ create({email :"test@getMaxListeners.com" , password : "123456"});
+ const users = repo.getAll();
+ console.log(users);
+}
 
 test();
